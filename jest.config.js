28d433a6de35dcce506e7cc7a8995179ab2cbd69
DrAVl <DrAV432@mail.ru>
2024-09-08 01:42:20 +0700

    jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-    testMatch: ['**/tests/**/*.test.js'],
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
-  };
+  roots: ['<rootDir>/tests'], // Указать корневую директорию для тестов
+  testEnvironment: 'node', // Указать тестовую среду
+  moduleDirectories: ['node_modules', 'src'], // Указать места для поиска модулей
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Настройка алиасов для более удобного импорта
+  },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest', // Трансформация jsx (для JavaScript и TypeScript)
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Файлы для настройки после окружения
+};
